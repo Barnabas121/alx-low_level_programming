5bcdeb3cc8db5df68ef01fd2273ef0b193fddbbb
Barnabas <barnabasabuye998@gmail.com>
2022-04-07 10:48:24 -0700

    this is to now the space of variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 /**
- * mailn - this is the containar
+ * main - this is the containar
+ * size of. how much
  * return: this will gize 0
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n""Size of an int: %d byte(s)""Size of a long int: %d byte(s)\n""Size of a long long int: %d byte(s)\n""Size of float: %d byte(s)");
+	printf("Size of a char: %d byte(s)\n", sizeof (char));
+	printf("Size of an int: %d byte(s)\n", sizeof (int));
+       printf("Size of a long int: %d byte(s)\n", sizeof (long int));
+       printf("Size of long long int: %d byte(s)\n", sizeof (long long int));
+       printf("Size of a float: %d byte(s)\n", sizeof (float));
 	return (0);
 }
