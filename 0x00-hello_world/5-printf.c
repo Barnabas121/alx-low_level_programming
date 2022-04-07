9ed0bafc43696f04899467eb2be74c3053530948
Barnabas <barnabasabuye998@gmail.com>
2022-04-07 10:03:58 -0700

    this is by using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - containar
+ * Return- output will be zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
