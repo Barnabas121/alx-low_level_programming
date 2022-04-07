0cc9bfdefbb61b39e72714cb1cf799e033c056d2
Barnabas <barnabasabuye998@gmail.com>
2022-04-07 09:53:51 -0700

    this is by ud using printf

---
+++ 0x00-hello_world/5-prinf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the containar
+ * Return - this will return 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
