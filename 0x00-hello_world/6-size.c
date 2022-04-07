d92f740a25f864e31a1497922bf53651e7050dd0
Barnabas <barnabasabuye998@gmail.com>
2022-04-07 10:30:38 -0700

    this is size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * mailn - this is the containar
+ * return: this will gize 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n""Size of an int: %d byte(s)""Size of a long int: %d byte(s)\n""Size of a long long int: %d byte(s)\n""Size of float: %d byte(s)");
+	return (0);
+}
