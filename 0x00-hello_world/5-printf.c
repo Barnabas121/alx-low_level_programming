e8d2207e9f07fe4749b9511d5cd073cece453ce8
Barnabas <barnabasabuye998@gmail.com>
2022-04-07 10:14:14 -0700

    this will printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - this is the containar
- * Return- output will be zero
+ * Return: output will be zero 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
