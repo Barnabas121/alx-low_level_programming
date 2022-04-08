cab93f6a81d86abb402f8c4eebf893416b37172e
Barnabas <barnabasabuye998@gmail.com>
2022-04-08 09:26:04 -0700

    c is awesome

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: that print 11 posiiible combination
+ * Return: Always 0
+ */
+int main(void)
+{
+	int firstNum = 0, secNum;
+
+	while (firsNum <= 99)
+	{
+		secNum = 0;
+		while (secNum <= 99)
+		{
+			if (firstNum != secNum)
+			{
+				putchar((firstNum / 10) + 48);
+				putchar((firstNum % 10) + 48);
+				putchar(' ');
+				putchar((secNum / 10) + 48);
+				putchar((SecNum % 10) + 48);
+				if (firstNum != 98 || secNum != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++secNum;
+		}
+		++firstNum;
+	}
+	putchar('\n');
+	return (0);
+}
