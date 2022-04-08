0a203cd1be62e8db696d3b8306b1dc94a1ed93a5
Barnabas <barnabasabuye998@gmail.com>
2022-04-08 15:16:03 -0700

    this isc

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,30 +6,27 @@
  */
 int main(void)
 {
-	int firstNum = 0, secNum;
+	 int num1, num2;
 
-	while (firstNum <= 99)
-	{
-		secNum = 0;
-		while (secNum <= 99)
-		{
-			if (firstNum != secNum)
-			{
-				putchar((firstNum / 10) + 48);
-				putchar((firstNum % 10) + 48);
-				putchar(' ');
-				putchar((secNum / 10) + 48);
-				putchar((secNum % 10) + 48);
-				if (firstNum != 98 || secNum != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			++secNum;
-		}
-		++firstNum;
-	}
-	putchar('\n');
-	return (0);
+	  for (num1 = 0; num1 <= 98; num1++)
+		   {
+			     for (num2 = num1 + 1; num2 <= 99; num2++)
+				       {
+					          putchar((num1 / 10) + '0');
+						     putchar((num1 % 10) + '0');
+						        putchar(' ');
+							   putchar((num2 / 10) + '0');
+							      putchar((num2 % 10) + '0');
+
+							         if (num1 == 98 && num2 == 99)
+									     continue;
+
+								    putchar(',');
+								       putchar(' ');
+								         }
+			      }
+
+	   putchar('\n');
+
+	    return (0);
 }
