6b8b951fb413f4f083378cf27d2a76fabccf92ed
Barnabas <barnabasabuye998@gmail.com>
2022-04-12 07:29:43 -0700

    this is my putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - this is the container
+ * description this is to print putchar
+ * Return: Always 0
+ */
+int main()
+{
+	char str[] = "_putchar";
+	
+	_putchar(strg[]);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+this is another c tutorial
