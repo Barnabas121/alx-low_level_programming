fdab9bb699237ff4207ef96973fda2abc85c6c5c
Barnabas <barnabasabuye998@gmail.com>
2022-04-12 09:55:10 -0700

    this is greate

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include"main.c"
+/**
+ * times_table - prints the 9 time table
+ */
+void times_table(void)
+{
+	int num, num1, num2;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(',');
+		_putchar(' ');
+
+		num3 = num * num2;
+
+		if (num3 <= 9)
+			_putchar(' ');
+		else
+			_putchar((num3 / 10 + '0');
+			_putchar((num3 % 10) + '0');
+	}
+}
+
