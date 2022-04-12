9d64d894c84b102e1308d36ccba00e2df336e315
Barnabas <barnabasabuye998@gmail.com>
2022-04-12 08:14:28 -0700

    this is good

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * print - alphabet utilize on the _putchar function to print
+ * print_alphabet - utilize on the _putchar function to print
  * the alphabet a - z
  * print_alphabet: to print a-z
  */
