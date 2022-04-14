2aa1d6df0bb2e3f551d6cf6d53efa7172981a9d4
Barnabas <barnabasabuye998@gmail.com>
2022-04-14 20:25:45 +0300

    this is awesome

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
  */
 void print_numbers(void)
 {
-	int c;
-
-	for (c = 0; c < 10; c++)
-	{
-		_putchar("%d", c);
-	}
-	return (0);
+	int c = 0;
+	
+	do {
+		_putchar(c + 48);
+		c++;
+	} while (c >= 0 && c <= 9);
+	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+include"main.h"
+/**
+ * print_most_numbers -this is from 0 -9
+ * Return: this is 0
+ */
+void print_most_numbers(void)
+{
+	int c = 0;
+	
+	for (c = 0; c <= 9; ++c)
+	{
+		if (c == 2 || c == 4)
+			continue;
+		_putchar(c + 48);
+	}
+	_putchar('\n');
+} 
