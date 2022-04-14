8adcfa0b888e7da6abf536880326113d1daaa01d
Barnabas <barnabasabuye998@gmail.com>
2022-04-14 17:55:19 +0300

    this is awesome

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,11 @@
-#include"main.h"
+#include "main.h"
+
 /**
- * _isdigit - entry point
- * @c: is for int
- * Return: 0
+ * _isdigit - this is for number from 0 to 9
+ * @c: this is for int variable
+ * Return: this will return 0
  */
-int _isdigit(int c);
+int _isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
 }
