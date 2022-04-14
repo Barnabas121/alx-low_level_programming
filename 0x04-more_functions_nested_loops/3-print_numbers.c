6611a5f92e90c7d962164c89090969b7bf46ee56
Barnabas <barnabasabuye998@gmail.com>
2022-04-15 02:52:22 +0300

    this is good

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,18 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * print_numbers -this is from 0 -9
- * Return: this is 0
+ * print_numbers - print from 0- 9 only using putchar
+ *
+ * Return: always 0
  */
+
 void print_numbers(void)
 {
-	int b = 0;
+	int num = 0;
 
 	do {
-		_putchar (b + 48);
-		b++;
-	} while (b >= 0; &&b <= 9)
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
