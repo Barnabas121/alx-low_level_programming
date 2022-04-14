6337efad1603cdd0f9a5b7ddebbd745034e395c4
Barnabas <barnabasabuye998@gmail.com>
2022-04-14 19:00:57 +0300

    this is awesome

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * more_numbers -this is from 0 -9
+ *
+ * Return: this is 0
+ */
+void more_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar("%d", c);
+	}
+	return (0);
+}
