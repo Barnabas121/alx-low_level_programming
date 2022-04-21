56fa6fb701629d549c8af08871d5a2b9588e6219
Barnabas <barnabasabuye998@gmail.com>
2022-04-21 20:19:22 +0300

    this is c on more pointer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,26 @@
-#include "main.h"
 /**
- * _strcat - this is to contcate strings
+ * _strcat - function that concatenates
+ *          two strings.
  *
- * @dest: represent destination
- * @src: represent source
- * Return: 0
- */
-char *_strcat(char *dest, char *src);
-{
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
 
-	_putchar("%s\n", s1);
-	_putchar("%s", s2);
-	_putchar(s1, s2);
-	_putchar("%s", s1);
-	_putchar("%s", s2);
-	return (0);
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+	/*find the size of dest array*/
+	while (dest[c])
+		c++;
+
+	/* iterate through each src array value without the null byte*/
+	for (c2 = 0; src[c2] ; c2++)
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+		dest[c++] = src[c2];
+
+	return (dest);
 }
