61995b78c245a4c97287fab8546a2e76603f5fa3
Barnabas <barnabasabuye998@gmail.com>
2022-05-24 19:00:14 +0300

    correcting error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,37 +1,28 @@
+#include <stdlib.h>
 #include "lists.h"
 /**
- * delete_nodeint_at_index - Deletes a node at a designated index
- * @head: Pointer to the beginning of the list
- * @index: Index of the node to be freed
- * Return: 1 if susccessful, -1 if it fails
+ * delete_nodeint_at_index - Deletes the node at a given index.
+ * @head: The first element of the list.
+ * @index: The position of the node to delete.
+ * Return: 1 if failure. -1 otherwise.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *tmp;
-	listint_t *node;
+	unsigned int i = 0;
+	listint_t *tmp, *tmp2;
 
 	tmp = *head;
-	if (head == NULL || *head == NULL)
-		return (-1);
-	for (i = 0; i < index - 1 && tmp != NULL && index != 0; i++)
+	while ((tmp->next != NULL) && (i + 1 < index))
+	{
 		tmp = tmp->next;
-	if (tmp == NULL)
-		return (-1);
-	if (index == 0)
-	{
-		node = tmp->next;
-		free(tmp);
-		*head = node;
+		i++;
 	}
-	else
+	if (i + 1 < index)
 	{
-		if (tmp->next == NULL)
-			node = tmp->next;
-		else
-			node = tmp->next->next;
-		free(tmp->next);
-		tmp->next = node;
+		tmp2 = tmp->next;
+		tmp->next = tmp->next->next;
+		free(tmp2);
+		return (1);
 	}
-	return (1);
+	return (-1);
 }
